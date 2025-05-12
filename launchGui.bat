cef9dacca4bb5f1b54f1c1b9e6694b250ec2e1d5
Ferne <a3230269751@163.com>
2025-05-12 23:19:18 +0800

    5/12-2319

--- launchGui.bat
+++ launchGui.bat
@@ -1,10 +1,10 @@
 @echo off
 
 :: TI SDK nw.exe GUI launcher
-set NW_EXECUTOR="E:\TI\sysconfig\nw\nw.exe"
+set NW_EXECUTOR="E:/TI/sysconfig/nw/nw.exe"
 :: TI SDK sysconfig 
-set SYSCONF="E:\TI\sysconfig"
+set SYSCONF="E:/TI/sysconfig"
 :: Project path
-set SYSCONF_FILE="E:\Interests\Code\micro_chip\jlc_training_course\course_1_pid_pcb_ti_project\ti-pid\empty\empty.syscfg"
+set SYSCONF_FILE="./empty/empty.syscfg"
 
 %NW_EXECUTOR% %SYSCONF% %SYSCONF_FILE%
